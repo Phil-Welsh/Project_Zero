e34670f56273999e0494677507e088d1d2ca8b47
Phil-Welsh <pwelsh8@gmail.com>
2021-09-20 15:33:00 -0700

    Set up containers, titles, buttons, stats, img, and progress bar in HTML. Added progress bar CSS. Set jQuery variables.

--- scripts/app.js
+++ scripts/app.js
@@ -1,1 +1,18 @@
 // Phil Welsh
+
+// Set jQuery variables
+$nameInputButton = $('#nameInputButton')
+$eatButton = $('#eatButton')
+$setUpCampButton = $('setUpCampButton')
+$playCardsButton = $('playCardsButton')
+
+$name = $('#nameInput')
+
+$snailImg = $('#snail')
+
+// Functions I need
+
+// 1. Save input name to variable
+// 2. Push up title after hitting "Let's go!"
+//          a. Hide naming box and unhide game
+// 3. 

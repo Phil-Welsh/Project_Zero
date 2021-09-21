2ccd9f2e4d5a7be6a5437cc40e912c58b85f90cf
Phil-Welsh <pwelsh8@gmail.com>
2021-09-20 22:22:29 -0700

    Added health count method to snail object

--- scripts/app.js
+++ scripts/app.js
@@ -1,18 +1,41 @@
 // Phil Welsh
 
-// Set jQuery variables
-$nameInputButton = $('#nameInputButton')
-$eatButton = $('#eatButton')
-$setUpCampButton = $('setUpCampButton')
-$playCardsButton = $('playCardsButton')
-
-$name = $('#nameInput')
-
-$snailImg = $('#snail')
-
 // Functions I need
-
-// 1. Save input name to variable
 // 2. Push up title after hitting "Let's go!"
 //          a. Hide naming box and unhide game
-// 3. 
+// 3. +1 on stats every 30 seconds
+// 4. -1 on stats every time respective button is pushed
+// 5. Progress bar that increases every 30 seconds
+// 6. Replay button when stats get to 10
+// 7. Animate snail getting fuzzier as he gets hungrier
+
+// Set jQuery variables
+const $nameInputButton = $('#nameInputButton')
+const $eatButton = $('#eatButton')
+const $setUpCampButton = $('#setUpCampButton')
+const $playCardsButton = $('#playCardsButton')
+const $nameInput = $('#nameInput')
+const $snailImg = $('#snail')
+const $hunger = $('hungerLi')
+
+// Snail object
+const snail = {
+    // Saves name input to object
+    name: () => {
+        snailName = $nameInput.val()
+        console.log(snailName)
+    },
+
+    // Health counter
+    hunger: () => {
+        snailHunger = 0
+        window.setInterval(function() {
+            snailHunger += 1
+            console.log(this.snailHunger)},
+            10000)
+        },
+}
+
+// Add event listeners
+$nameInputButton.on('click', snail.name)
+$nameInputButton.on('click', snail.hunger)
